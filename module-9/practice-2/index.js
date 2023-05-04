c578c90ffa97a93435050f0c71175f581a8616d6
YanaKonovalova93 <mikobacteria@gmail.com>
2023-05-04 22:33:57 +0300

    +

--- module-9/practice-2/index.js
+++ module-9/practice-2/index.js
@@ -4,39 +4,66 @@
     3. Change mock data to real one
 */
 
+
+
+// const addAuthor = (event) => {
+//   event.preventDefault();
+//   const authorName = event.target.authorName.value;
+
+//   if (authorName) {
+//     const delay = Math.random() > 0.5 ? 1500 : 2500;
+//     api.addAuthor(authorName, delay);
+//     renderAuthors();
+//   }
+// };
+
+// const renderAuthors = () => {
+//   api
+//     .getAuthors()
+//     .then((result) => {
+//       if (Array.isArray(result)) {
+//         authorsEl.innerHTML = "";
+//         authorsNumberEl.textContent = `There are ${result.length} authors in the system`;
+
+//         result.forEach((item) => {
+//           const el = document.createElement("li");
+//           el.textContent = item;
+//           authorsEl.appendChild(el);
+//         });
+//       }
+//     })
+//     .catch((err) => (authorsEl.innerHTML = err));
+// };
+
+// addAuthorFormEl.addEventListener("submit", addAuthor);
+
 import api from "./scripts/api.js";
 
 const addAuthorFormEl = document.getElementById("addAuthorForm");
 const authorsNumberEl = document.getElementById("authorsNumber");
 const authorsEl = document.getElementById("authors");
 
-const addAuthor = (event) => {
+const addAuthorFormHandler = (event) => {
   event.preventDefault();
   const authorName = event.target.authorName.value;
 
   if (authorName) {
-    const delay = Math.random() > 0.5 ? 1500 : 2500;
-    api.addAuthor(authorName, delay);
+    api.addAuthor(authorName);
     renderAuthors();
   }
 };
 
 const renderAuthors = () => {
-  api
-    .getAuthors()
-    .then((result) => {
-      if (Array.isArray(result)) {
-        authorsEl.innerHTML = "";
-        authorsNumberEl.textContent = `There are ${result.length} authors in the system`;
+  api.getAuthors().then((authors) => {
+    authorsEl.innerHTML = "";
+    authorsNumberEl.textContent = `There are ${authors.length} authors in the system`;
+    authors.forEach((authorName) => {
+      const el = document.createElement('li');
+      el.textContent = authorName.trim();
+      authorsEl.appendChild(el);
+      // or innerHtml
 
-        result.forEach((item) => {
-          const el = document.createElement("li");
-          el.textContent = item;
-          authorsEl.appendChild(el);
-        });
-      }
     })
-    .catch((err) => (authorsEl.innerHTML = err));
-};
-
-addAuthorFormEl.addEventListener("submit", addAuthor);
+  });
+}
+addAuthorFormEl.addEventListener("submit", addAuthorFormHandler);
--- module-9/practice-2/scripts/api.js
+++ module-9/practice-2/scripts/api.js
@@ -15,3 +15,51 @@ export default {
   addAuthor,
   getAuthors,
 };
+
+// const authors = [];
+
+// const addAuthor = (authorName, delay) => {
+//   authorName.push(
+//     new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         authorName ? resolve(authorName) : reject("No name");
+//       }, delay);
+//     })
+//   );
+// };
+
+// const getAuthors = () => Promise.all(authors);
+
+// export default {
+//   addAuthor,
+//   getAuthors,
+// };
+
+
+// промисифікація--------------
+
+function sum(x, y) {
+  const isSuccess = x > 0;
+
+  if (isSuccess) {
+    console.log("Success, hooray");
+    return x + y;
+  } else {
+    console.log("Bad luck");
+  }
+}
+
+function sum(x, y) {
+  const isSuccess = x > 0;
+  // if (isSuccess) {
+  //   return Promise.resolve(x+y)
+  // } else {
+  //   return Promise.reject("failed");
+  // }
+
+  return isSuccess ? Promise.resolve(x + y) : Promise.reject("failed");
+}
+
+sum()
+  .then((greeting) => console.log(greeting))
+  .catch((error) => console.log(error));
